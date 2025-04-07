d1b7b1d81e96a2d67ba0f6f6ee248ef9190270b5
pritesh511 <patelpritesh313@gmail.com>
2025-04-07 14:35:42 +0530

    Implement middlwear verify token

--- src/middlewares/checkToken.ts
+++ src/middlewares/checkToken.ts
@@ -1,9 +1,27 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { throwError } from "../helper/common";
 
 export const validateToken = (
   req: Request,
-  res: Response,
+  res: any,
   next: NextFunction
 ) => {
-  next();
+  const token = (req.headers?.token as string) || "";
+
+  // check if token is present
+  if (!token) {
+    return throwError(res, "Invalid Token", 400);
+  }
+
+  // verify token
+  try {
+    const secretKey = process.env.TOKEN_SECRET as string;
+    const user = jwt.verify(token, secretKey);
+
+    (req as any).user = user;
+    next();
+  } catch (error) {
+    return throwError(res, "Unauthorized", 401);
+  }
 };
