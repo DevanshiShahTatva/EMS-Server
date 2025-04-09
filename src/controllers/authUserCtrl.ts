7e948c03da3da1e438e241e3cdd8d176614e2684
pritesh511 <patelpritesh313@gmail.com>
2025-04-09 16:03:33 +0530

    handle admin and normal user

--- src/controllers/authUserCtrl.ts
+++ src/controllers/authUserCtrl.ts
@@ -3,6 +3,9 @@ import { ApiResponse, create, findOne, throwError } from "../helper/common";
 import { HTTP_STATUS_CODE } from "../utilits/enum";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const registerUser = async (req: Request, res: Response) => {
   try {
@@ -63,38 +66,68 @@ export const loginUser = async (req: Request, res: Response) => {
       );
     }
 
-    // validation for if email is exists
-    const findUser = await findOne("User", { email: email }, sort);
-    if (!findUser) {
-      return throwError(res, "Email not found", HTTP_STATUS_CODE.NOT_FOUND);
+    // check is admin user login
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const adminUser = {
+        name: "Event Manager",
+        email: "admin@evently.com",
+        role: "admin",
+      };
+
+      const token = jwt.sign(adminUser, process.env.TOKEN_SECRET!, {
+        expiresIn: "1d",
+      });
+
+      const userDataWithToken = {
+        ...adminUser,
+        token: token,
+      };
+      rcResponse.data = userDataWithToken;
+      rcResponse.message = "You have login successfully."
+      return res.status(rcResponse.status).send(rcResponse);
+    } else {
+      // validation for if email is exists
+      const findUser = await findOne("User", { email: email }, sort);
+      if (!findUser) {
+        return throwError(res, "Email not found", HTTP_STATUS_CODE.NOT_FOUND);
+      }
+
+      // encrypt the password
+      const isValidPassword = await bcryptjs.compare(
+        password,
+        findUser.password
+      );
+
+      if (!isValidPassword) {
+        return throwError(
+          res,
+          "Invalid Password",
+          HTTP_STATUS_CODE.BAD_REQUEST
+        );
+      }
+
+      const tokenUser = {
+        _id: findUser._id,
+        name: findUser.name,
+        email: findUser.email,
+        role: "user",
+      };
+
+      const token = jwt.sign(tokenUser, process.env.TOKEN_SECRET!, {
+        expiresIn: "1d",
+      });
+
+      const userDataWithToken = {
+        ...tokenUser,
+        token: token,
+      };
+      rcResponse.data = userDataWithToken;
+      rcResponse.message = "You have login successfully."
+      return res.status(rcResponse.status).send(rcResponse);
     }
-
-    // encrypt the password
-    const isValidPassword = await bcryptjs.compare(
-      password,
-      findUser.password
-    );
-
-    if (!isValidPassword) {
-      return throwError(res, "Invalid Password", HTTP_STATUS_CODE.BAD_REQUEST);
-    }
-
-    let tokenUser = {
-      _id: findUser._id,
-      name: findUser.name,
-      email: findUser.email,
-    };
-
-    const token = jwt.sign(tokenUser, process.env.TOKEN_SECRET!, {
-      expiresIn: "1d",
-    });
-
-    const userDataWithToken = {
-      ...tokenUser,
-      token: token,
-    };
-    rcResponse.data = userDataWithToken;
-    return res.status(rcResponse.status).send(rcResponse);
   } catch (err) {
     return throwError(res);
   }
