f674cfab56f0c5ce82a4725f6ec25988c5ebeda2
Devanshi <60215921+DevanshiShah29@users.noreply.github.com>
2025-04-09 16:54:00 +0530

    Update eventsRoutes.ts

--- src/routes/eventsRoutes.ts
+++ src/routes/eventsRoutes.ts
@@ -1,10 +1,30 @@
 import { Router } from "express";
 import { validateToken } from "../middlewares/checkToken";
-import { getEvents, postEvent } from "../controllers/eventCtrl";
+import {
+  deleteEvent,
+  getEvents,
+  postEvent,
+  putEvent,
+} from "../controllers/eventCtrl";
+import multer from "multer";
+
+const upload = multer();
 
 const eventsRoutes = Router();
 
-eventsRoutes.post("/event", validateToken, postEvent);
-eventsRoutes.get("/events", validateToken, getEvents);
+eventsRoutes.post(
+  "/",
+  validateToken,
+  upload.array("images", 5),
+  postEvent
+);
+eventsRoutes.get("/", validateToken, getEvents);
+eventsRoutes.put(
+  "/:id",
+  validateToken,
+  upload.array("images", 5),
+  putEvent
+);
+eventsRoutes.delete("/:id", validateToken, deleteEvent);
 
 export default eventsRoutes;
