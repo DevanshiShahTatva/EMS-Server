836b1d2ce13fdea678b92f489074a7deac9c3221
jay-tatvasoft <jay.vadgama@tatvasoft.com>
2025-05-07 12:16:05 +0530

    Added dashboard api filter changes

--- src/routes/dashboardRoutes.ts
+++ src/routes/dashboardRoutes.ts
@@ -16,19 +16,11 @@ const dashboardRoutes = Router();
 
 dashboardRoutes.get("/dashboard-overview", validateAdminToken, dashboardOverview);
 dashboardRoutes.get("/top-liked-events", validateAdminToken, topLikedEvents);
-dashboardRoutes.get("/total-revenue", validateAdminToken, totalRevenue);
-dashboardRoutes.get(
-  "/average-booking-value",
-  validateAdminToken,
-  totalBookingValue
-);
+dashboardRoutes.get("/total-revenue", validateAdminToken, totalRevenue); // filter
+dashboardRoutes.get("/average-booking-value", validateAdminToken, totalBookingValue); // filter
 dashboardRoutes.get("/top-revenue-events", validateAdminToken, topRevenueEvents);
 dashboardRoutes.get("/repeat-customers", validateAdminToken, repeateCustomer);
-dashboardRoutes.get(
-  "/bookings-by-ticket-type",
-  validateAdminToken,
-  bookingsByTicketType
-);
+dashboardRoutes.get("/bookings-by-ticket-type", validateAdminToken, bookingsByTicketType);
 dashboardRoutes.get("/bookings-time-trends", validateAdminToken, bookingsTimeTrends);
 dashboardRoutes.get("/top-locations", validateAdminToken, topLocations);
 
