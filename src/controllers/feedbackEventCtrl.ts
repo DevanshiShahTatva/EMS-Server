d842e3a6e8f6fb1df5a7ac0fd0467364f42bfa5d
Yash Mathur <yashmthr65@gmail.com>
2025-05-09 15:44:44 +0530

    User Id changes

--- src/controllers/feedbackEventCtrl.ts
+++ src/controllers/feedbackEventCtrl.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
 import {
   ApiResponse,
+  getUserIdFromToken,
   throwError,
 } from "../helper/common";
 import Event from "../models/event.model";
 import { FeedbackModel } from "../models/feedback.model";
-export const feedbackEvent = async (req: any, res: Response): Promise<void> => {
+import { Types } from "mongoose";
+import User from "../models/signup.model";
+export const feedbackEvent = async (req: Request, res: Response): Promise<void> => {
     try {
       const rcResponse = new ApiResponse();
       const { rating, description } = req.body
@@ -20,9 +23,14 @@ export const feedbackEvent = async (req: any, res: Response): Promise<void> => {
         res.status(rcResponse.status).json({ message: 'Event not found.' })
         return
       }
-      
-      const name = req.user.name;
-      const email = req.user.email;    
+      //Checking if user is available
+      const userId = new Types.ObjectId(getUserIdFromToken(req));
+      if(!userId){
+        res.status(rcResponse.status).json({message:'User not found.'})
+      }
+      const user = await User.findById(userId);
+      const name = user.name;
+      const email = user.email;    
       const feedback = await FeedbackModel.create({
         name,
         email,
