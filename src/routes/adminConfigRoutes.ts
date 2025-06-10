5b418bb113811e4c2a7cfb4a98dd04aaae91bf85
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-06-10 16:11:39 +0530

    get cancel charge make public

--- src/routes/adminConfigRoutes.ts
+++ src/routes/adminConfigRoutes.ts
@@ -4,7 +4,7 @@ import { putCancelCharge, getCancelCharge } from "../controllers/adminConfigCtrl
 
 const router = Router();
 
-router.get("/cancel-charge", validateToken, getCancelCharge);
+router.get("/cancel-charge", getCancelCharge);
 router.put("/cancel-charge", validateAdminToken, putCancelCharge);
 router.get("/cancel-charge", validateAdminToken, getCancelCharge);
 
